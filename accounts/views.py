9e38c1c9634a18bc0fcbc5be24c2c4aec50be6e1
sudonetizen <sardortolibjonov1039@gmail.com>
2026-03-05 03:03:33 +0500

    add custom login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+class CustomLoginForm(AuthenticationForm):
+    pass
+
+class LoginView(View):
+    def get(self, request):
+        form = CustomLoginForm()
+        return render(request, "../templates/registration/login.html", context={"form": form})
+
+    def post(self, request):
+        form = CustomLoginForm(request, request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("home")
+
+        return render(request, "../templates/registration/login.html", context={"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
